106fe768d44068ad08f03ee941b990127677fe04
Lucas Dietrich <ld.adecy@gmail.com>
2024-09-15 22:01:00 +0200

    Enable USB device stack
    * Use USB CDC ACM for logging

--- src/main.c
+++ src/main.c
@@ -4,33 +4,32 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <zephyr/kernel.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/gpio.h>
-#include <zephyr/sys/util.h>
-#include <zephyr/sys/printk.h>
 #include <inttypes.h>
 
-#include <caniot/device.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/usb/usb_device.h>
+LOG_MODULE_REGISTER(usb, LOG_LEVEL_DBG);
 
+#include <caniot/device.h>
 #include <test.h>
 
-#define SLEEP_TIME_MS	1
+#define SLEEP_TIME_MS 1
 
-#define SW0_NODE	DT_ALIAS(sw0)
+#define SW0_NODE DT_ALIAS(sw0)
 #if !DT_NODE_HAS_STATUS(SW0_NODE, okay)
 #error "Unsupported board: sw0 devicetree alias is not defined"
 #endif
-static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios,
-							      {0});
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(SW0_NODE, gpios, {0});
 static struct gpio_callback button_cb_data;
 
+static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0});
 
-static struct gpio_dt_spec led = GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios,
-						     {0});
-
-void button_pressed(const struct device *dev, struct gpio_callback *cb,
-		    uint32_t pins)
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
 	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
 }
@@ -40,23 +39,21 @@ int button_init(void)
 	int ret;
 
 	if (!gpio_is_ready_dt(&button)) {
-		printk("Error: button device %s is not ready\n",
-		       button.port->name);
+		printk("Error: button device %s is not ready\n", button.port->name);
 		return 0;
 	}
 
 	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
 	if (ret != 0) {
-		printk("Error %d: failed to configure %s pin %d\n",
-		       ret, button.port->name, button.pin);
+		printk("Error %d: failed to configure %s pin %d\n", ret, button.port->name,
+			   button.pin);
 		return 0;
 	}
 
-	ret = gpio_pin_interrupt_configure_dt(&button,
-					      GPIO_INT_EDGE_TO_ACTIVE);
+	ret = gpio_pin_interrupt_configure_dt(&button, GPIO_INT_EDGE_TO_ACTIVE);
 	if (ret != 0) {
-		printk("Error %d: failed to configure interrupt on %s pin %d\n",
-			ret, button.port->name, button.pin);
+		printk("Error %d: failed to configure interrupt on %s pin %d\n", ret,
+			   button.port->name, button.pin);
 		return 0;
 	}
 
@@ -65,15 +62,15 @@ int button_init(void)
 	printk("Set up button at %s pin %d\n", button.port->name, button.pin);
 
 	if (led.port && !gpio_is_ready_dt(&led)) {
-		printk("Error %d: LED device %s is not ready; ignoring it\n",
-		       ret, led.port->name);
+		printk("Error %d: LED device %s is not ready; ignoring it\n", ret,
+			   led.port->name);
 		led.port = NULL;
 	}
 	if (led.port) {
 		ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT);
 		if (ret != 0) {
-			printk("Error %d: failed to configure LED device %s pin %d\n",
-			       ret, led.port->name, led.pin);
+			printk("Error %d: failed to configure LED device %s pin %d\n", ret,
+				   led.port->name, led.pin);
 			led.port = NULL;
 		} else {
 			printk("Set up LED at %s pin %d\n", led.port->name, led.pin);
@@ -83,17 +80,38 @@ int button_init(void)
 	return ret;
 }
 
+void usb_status_cb(enum usb_dc_status_code cb_status, const uint8_t *param)
+{
+	printk("USB status: %d\n", cb_status);
+}
 
-static struct caniot_device device;
+static int usb_init(void)
+{
+	int ret;
+
+	ret = usb_enable(usb_status_cb);
+	if (ret != 0) {
+		printk("Failed to enable USB");
+		goto exit;
+	}
+
+exit:
+	return ret;
+}
 
 int main(void)
 {
 	printk("Hello from teal device!\n");
 
+	// static struct caniot_device device;
 	// caniot_app_init(&device);
 
 #if defined(CONFIG_TEST)
 	test_init();
+#endif
+
+#if defined(CONFIG_USB_DEVICE_STACK)
+	usb_init();
 #endif
 
 	button_init();
