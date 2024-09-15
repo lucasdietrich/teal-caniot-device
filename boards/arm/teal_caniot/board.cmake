5dabd5587bb7ee6dab4034ed795964cf0333061f
Lucas Dietrich <ld.adecy@gmail.com>
2024-09-16 00:56:02 +0200

    Minor changes

--- boards/arm/teal_caniot/board.cmake
+++ boards/arm/teal_caniot/board.cmake
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2024 Lucas Dietrich <ld.adecy@gmail.com>
 #
-# SPDX-License-Identifier: Apache-4.0
+# SPDX-License-Identifier: Apache-2.0
 #
 board_runner_args(jlink "--device=stm32l462re" "--iface=swd" "--speed=4000" "--reset-after-load")
 
