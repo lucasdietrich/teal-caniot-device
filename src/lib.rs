8a1c4153a50be1a4fa4f9465d7c80077548bd2ff
Lucas Dietrich <ld.adecy@gmail.com>
2025-01-04 20:09:55 +0100

    Add Rust build configuration and test rust mutex

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,26 @@ use zephyr::printkln;
 // symbols from the crate are directly used.
 extern crate zephyr;
 
+pub struct MyData {
+    number: u32,
+}
+
 #[no_mangle]
 extern "C" fn rust_main() {
     printkln!("Hello world from Rust on {}",
               zephyr::kconfig::CONFIG_BOARD);
+
+    let data = MyData { number: 42 };
+
+    printkln!("Number is {}", data.number);
+
+    let var = zephyr::sync::Mutex::new(data);
+    let guard = var.lock();
+    
+    if let Ok(mut guard) = guard {
+        guard.number += 1;
+        printkln!("Number is {}", guard.number);
+    } else {
+        printkln!("Mutex lock failed");
+    }
 }
